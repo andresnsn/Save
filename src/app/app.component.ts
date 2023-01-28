a0bbe185b3a4db3bb888d6af77455e81ff045e94
André Ribeiro <andre.sn@live.com>
2023-01-28 04:12:16 -0300

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,21 +20,36 @@ export class AppComponent {
 
   private errorCount = 0;
 
-  public errorRegister: any[] = []
+  public errorRegister: any[] = [];
+
+  public successRegister: any[] = [];
 
 
   @HostListener('document:keypress', ['$event'])
-  test(event: any): any{
+  eventHandler(event: any): any{
 
-    if(this.position > this.size) { return };
+    if(this.position >= this.size) {
+
+      let subtraction = this.text.length - this.errorCount;
+      let calc = (subtraction / this.text.length) * 100;
+
+      calc == 100? alert("Você é um Hokage!") :
+      calc < 99.9 && calc > 75? alert("Você é um Jonnin!") :
+      calc < 74.9 && calc > 50? alert("Você é um Chunnin!") :
+      calc < 49.9 && calc > 25? alert("Você é um Gennin!") :
+      calc < 25? alert("Você é um bosta"!) : null
+
+
+    };
 
     this.isWrong = event.key == this.text[this.position] ? false : true;
 
-    !this.isWrong? this.position = ++this.position : (++this.errorCount, this.errorRegister.push(this.text[this.position]+this.position ));
-
-    console.log(this.errorRegister);
-
-    console.log(this.errorRegister.includes('o1'))
+    !this.isWrong? this.position =
+    (
+      this.errorRegister.includes(this.text[this.position]+this.position)? null : this.successRegister.push(this.text[this.position]+this.position),
+      ++this.position
+    ) :
+    (++this.errorCount, this.errorRegister.push(this.text[this.position]+this.position));
 
     this.currentLetter = this.text[this.position];
 
@@ -48,8 +63,6 @@ export class AppComponent {
     let indexedLetters: any[] = [];
 
     splittedText.forEach((value, index) => { indexedLetters.push({value, index}) })
-
-    console.log(indexedLetters)
 
     return indexedLetters;
 
